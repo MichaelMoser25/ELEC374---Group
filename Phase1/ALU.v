853598318722b9e25c1663b6a437b5e54861cd66
br15an <bgreene0009@gmail.com>
2025-01-27 16:02:00 -0500

    add, sub using 32 bit CLA

--- Phase1/ALU.v
+++ Phase1/ALU.v
@@ -1,20 +1,25 @@
 module ALU ( 
 input wire [31:0] A, B,
 input wire [3:0]  op,
-output reg [7:0]  result
+output reg [31:0]  result
 );
-
-
+wire Cout;
 /* OPERATORS:
 	0 : or
 	1 : and
 	2 : not
+	3 : add
+	4 : sub
 */
+ 
 always @(*) begin
 	case(op)
 				0		: result = (A|B);
 				1		: result = (A&B);
-				2		: result = !A;
+				2		: result = ~A;
+				3		: CLA_32bit adder(A, B, 0, 0, result, Cout);
+				4		: CLA_32bit subtractor(A, B, 1, 0, result, Cout);
+				
 
 				default: result = A;
 	endcase
