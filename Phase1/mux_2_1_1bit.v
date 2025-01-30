00d20bd9a7db65f07f9e133db9bdbf42638edb9c
Braeden Boyle <2braedenboyle@gmail.com>
2025-01-30 16:33:02 -0500

    fixed syntax error in 2-1 mux

--- Phase1/mux_2_1_1bit.v
+++ Phase1/mux_2_1_1bit.v
@@ -1,6 +1,6 @@
 module mux_2_1_1bit(
-	input wire data_1, data_0, select;
-	output wire mux_out;
+	input wire data_1, data_0, select,
+	output wire mux_out
 );
 
 	assign mux_out = select ? data_1 : data_0;
